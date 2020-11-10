04191cdf449699ade31f7bdf0bba771b24b73b74
Bruce Emmanuel Sueira <bruce.dts@gmail.com>
2020-11-10 10:27:36 -0300

    Adicionado a mensagem do Ten Paiva

--- messages.php
+++ messages.php
@@ -1,30 +1,35 @@
 <?php
 
 $messages = [
-  [
-    'name' => 'Jonatas Cavalcante',
-    'image' => 'jonatas.jpg',
-    'message' => 'Eu te falei que a seção de WEB era a melhor do NTS, mas você conseguiu deixar ela melhor ainda!
+    [
+        'name' => 'Jonatas Cavalcante',
+        'image' => 'jonatas.jpg',
+        'message' => '“Eu te falei que a seção de WEB era a melhor do NTS, mas você conseguiu deixar ela melhor ainda!
 Sua iniciativa, sua atenção para com todos nós e o carinho que demonstra através de suas ações são caraterísticas que eu admiro muito!
 Obrigado por todos os rolês e pela paciência de ficar me ouvindo falando tantas coisas aleatórias (sei que isso demanda energia hehehe)
 Meus parabéns por mais esse ano de vida! Desejo que essa data se repita muitas vezes, com saúde, paz e muitas conquistas, porque você merece e muito!
 Continue sendo essa pessoa atenciosa, gentil e prestativa, sempre levando carinho e cuidado aonde quer que vá!
-Um grande beijo um abraço bem apertado! Felicidades!'
-  ],
-  [
-  'name' => 'Histelen Leonardo',
-  'image' => 'histelen.jpg',
-  'message' => 'Feliz aniversário. Que Deus continue te abençoando hoje e sempre. Que sua personalidade e profissionalismo continuem sempre surpreendendo. Muita saúde e paz.'
-  ],
-  [
-  'name' => 'Bruce & Karol',
-  'image' => 'bruce&krol.jpg',
-  'message' => 'Priscila! Desejo que nesta data, onde a 26 anos você recebeu o maior presente de todos que é o dom da vida, seja repleta de felicidade e motivos para agradecer e festejar. Parabéns por ser essa pessoa atenciosa e dedicada, que faz por merecer todo o sucesso na vida pessoal e profissional. Feliz aniversário!'
-  ],
-  [
-    'name' => 'Rafael Tadeu',
-    'image' => 'rafael.jpg',
-    'message' => 'Parabéns Priscila! <br />
- Desejo que você celebre essa data por muitos anos, e que nunca lhe falte paz no coração, saúde, sorrisos no rosto e boas companhias. E que cada novo dia seja o renovar de esperança e entusiasmo, nessa breve viagem que é a vida'
-  ]
+Um grande beijo um abraço bem apertado! Felicidades!“'
+    ],
+    [
+        'name' => 'Histelen Leonardo',
+        'image' => 'histelen.jpg',
+        'message' => '“Feliz aniversário. Que Deus continue te abençoando hoje e sempre. Que sua personalidade e profissionalismo continuem sempre surpreendendo. Muita saúde e paz.“'
+    ],
+    [
+        'name' => 'Bruce & Karol',
+        'image' => 'bruce&krol.jpg',
+        'message' => '“Priscila! Desejo que nesta data, onde a 26 anos você recebeu o maior presente de todos que é o dom da vida, seja repleta de felicidade e motivos para agradecer e festejar. Parabéns por ser essa pessoa atenciosa e dedicada, que faz por merecer todo o sucesso na vida pessoal e profissional. Feliz aniversário!“'
+    ],
+    [
+        'name' => 'Rafael Tadeu',
+        'image' => 'rafael.jpg',
+        'message' => '“Parabéns Priscila! <br />
+ Desejo que você celebre essa data por muitos anos, e que nunca lhe falte paz no coração, saúde, sorrisos no rosto e boas companhias. E que cada novo dia seja o renovar de esperança e entusiasmo, nessa breve viagem que é a vida“'
+    ],
+    [
+        'name' => 'Guilherme de Paiva',
+        'image' => 'paiva.jpg',
+        'message' => '“Pri, quando vc veio para o NTS nós jamais imaginávamos a energia que vc traria junto! Vc eh luz pura de energia, e inundou o ambiente com mais amor, carinho e disposição (e haja disposição!!). Muito obrigado e um feliz aniversário!!!!! Você é sensacional!'
+    ]
 ];
